e282abcf44e14a8237f61d291c76c5163abbd619
santilb <santib@gmail.com>
2023-10-18 11:18:53 -0300

    entrega

--- src/config/configMongoDB.js
+++ src/config/configMongoDB.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 const config = {
   mongoDB: {
-    URL: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@coderhouse.ai8ozim.mongodb.net/?retryWrites=true&w=majority`,
+    URL: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@coderhouse.ai8ozim.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -12,9 +12,6 @@ const config = {
 };
 
 export const connectMongoDB = async () => {
-  console.log("Usuario "  + process.env.MONGO_USER)
-  console.log("Pass "  + process.env.MONGO_PASS)
-  console.log("Base  "  + process.env.DB_NAME)
   try {
     await mongoose.connect(config.mongoDB.URL, config.mongoDB.options);
     console.log("Connected to Mongo Atlas");
